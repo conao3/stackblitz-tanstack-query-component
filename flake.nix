2982b7bede97f375bc97313d7dd23c9162489def
conao3 <conao3@gmail.com>
2025-09-22 01:19:13 +0900

    add flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "stackblitz-tanstack-query-component";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-darwin"];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem = {
+        pkgs,
+        ...
+      }: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            biome.enable = true;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            biome
+          ];
+        };
+      };
+    };
+}
